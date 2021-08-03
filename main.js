e52b82cdfbad4ea0d4356407dde4a987aa0190bb
1 <tawers6103@gmail.com>
2021-08-03 19:55:38 +0300

    js3

--- main.js
+++ main.js
@@ -1,29 +1,8 @@
 //wВ index.html
 //1 получить массив объектов user с endpoint`а https://jsonplaceholder.typicode.com/users
 //2 Вывести id,name всех user в index.html. Отдельный блок для каждого user.
-//3 Добавить каждому блоку кнопку/ссылку , при клике на которую происходит переход на страницу user-details.html, которая имеет детальную информацию про объект на который кликнули
-// fetch('https://jsonplaceholder.typicode.com/users')
-//     .then(value => value.json())
-//     .then(users => {
-//         for (let user of users) {
-//             let userDiv = document.createElement('div');
-//             let a = document.createElement('a');
-//             let block = document.createElement('div')
-//
-//             let p = document.createElement('p')
-//             userDiv.append(p)
-//             userDiv.append(a)
-//             userDiv.append(block)
-//             a.innerText = 'press'
-//             //btn.innerText = 'press'
-//             //document.createElement('index.html')
-//             p.innerText = `${user.id} ${user.name}`
-//             a.onclick = function () {
-//                 a.href = 'user-details.html'
-//             }
-//             let target = document.getElementById('target');
-//             target.append(userDiv);
-//         }})
+//3 Добавить каждому блоку кнопку/ссылку , при клике на которую происходит переход на страницу user-d.html, которая имеет детальную информацию про объект на который кликнули
+
 const mainDiv = document.getElementById('users');
 
 fetch('https://jsonplaceholder.typicode.com/users')
@@ -32,22 +11,17 @@ fetch('https://jsonplaceholder.typicode.com/users')
         for (const user of users) {
             let userDiv = document.createElement('div');
             let detailsLink = document.createElement('a');
-
-            userDiv.innerText = user.name;
+            let h2 = document.createElement('h2')
+            h2.innerText = `${user.name}`
+            //userDiv.innerHTML = <h2>`${user.name}`</h2>
             detailsLink.innerText = 'Get info';
 
             detailsLink.href = `user-d.html?user=${JSON.stringify(user)}`;
-
+            userDiv.appendChild(h2)
             userDiv.appendChild(detailsLink);
             mainDiv.appendChild(userDiv);
         }
     });
-
-//На странице user-details.html:
-//4 Вывести всю, без исключения, информацию про объект user на кнопку/ссылку которого был совершен клик ранее.
-//5 Добавить кнопку "post of current user", при клике на которую, появляются title всех постов текущего юзера
-//(для получения постов используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
-//6 Каждому посту добавить кнопку/ссылку, при клике на которую происходит переход на страницу post-details.html, которая имеет детальную информацию про текущий пост.
 
 //На странице post-details.html:
 //7 Вывести всю, без исключения, информацию про объект post на кнопку/ссылку которого был совершен клик ранее.
@@ -55,7 +29,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
 
 //Стилизация проекта -
 //index.html - все блоки с user - по 2 в ряд. кнопки/ссылки находяться под информацией про user.
-//user-details.html - блок с информацией про user вверху страницы. Кнопка ниже, на 90% ширины страницы, по центру.
+//user-d.html - блок с информацией про user вверху страницы. Кнопка ниже, на 90% ширины страницы, по центру.
 //блоки с краткой информацией про post - в ряд по 5 объектов.
 //post-details.html - блок с информацией про пост вверху. Комментарии - по 4 в ряд.
 //Все без исключения элементы, который характеризируют user,post,comment  визуализировать, так, что бы было видно их блоки (дать задний фон + margin. Иными словами - крайне четкая сетка)
