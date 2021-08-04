c62cd1ad9498c15cc6de29b5d1537190f4ee30c5
1 <tawers6103@gmail.com>
2021-08-04 15:33:22 +0300

    js3

--- main.js
+++ main.js
@@ -2,9 +2,7 @@
 //1 получить массив объектов user с endpoint`а https://jsonplaceholder.typicode.com/users
 //2 Вывести id,name всех user в index.html. Отдельный блок для каждого user.
 //3 Добавить каждому блоку кнопку/ссылку , при клике на которую происходит переход на страницу user-d.html, которая имеет детальную информацию про объект на который кликнули
-
 const mainDiv = document.getElementById('users');
-
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(value => value.json())
     .then(users => {
